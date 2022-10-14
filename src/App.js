ac43c4403f6a955c52be2fe79cc9e2467d2293b1
rfreno <wintersbek@gmail.com>
2022-10-14 09:59:20 -0600

    commented logic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Square from "./Square";
 
 function App() {
+  // setup initial squares and player X vs O
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
   const handleClick = () => {
+    // reset board to initial squares and player
     setSquares(["", "", "", "", "", "", "", "", ""]);
     setPlayer(true);
   };
@@ -27,10 +29,11 @@ function App() {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
 
+      // if three squares in a row (any of the winning combos) have the same value, we have a winner!
       if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c])
         return `${arr[a]} won!`;
     }
-
+    // return this question until we have a winner
     return "Who will win?";
   };
 
@@ -38,6 +41,7 @@ function App() {
     <div className="App">
       <span>{calculateWinner(squares)}</span>
       <div className="container">
+        // set up game board - each square has index, value, and player attached
         {squares.map((squareValue, index) => (
           <Square
             setSquares={setSquares}
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Square = (props) => {
+  // when square is clicked -> if square has no value, check which player has clicked, set value at corresponding index, reset squares array, and reset player to the next
   const handleClick = () => {
     if (!props.squareValue) {
       if (props.player) {
